035c65bd1463e78e125a4c5b8512d0693c567f93
chinakids <494063570@qq.com>
2015-07-13 11:33:57 +0800

    将开发目录定位至 dev

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var yeoman = require('yeoman-generator'),
-    osLocale = require('os-locale'),
-    join = require('path').join,
-    _ = require('underscore'),
-    chalk = require('chalk'),
-    fs = require('fs');
+  osLocale = require('os-locale'),
+  join = require('path').join,
+  _ = require('underscore'),
+  chalk = require('chalk'),
+  fs = require('fs');
 
 module.exports = yeoman.generators.Base.extend({
-  constructor: function () {
+  constructor: function() {
     yeoman.generators.Base.apply(this, arguments);
 
 
@@ -30,21 +30,27 @@ module.exports = yeoman.generators.Base.extend({
     this.pkg = require('../package.json');
   },
 
-  askFor: function () {
+  askFor: function() {
     var done = this.async(),
-        _this = this,
-        baseLocale = JSON.parse(fs.readFileSync(join(_this.sourceRoot(), '../language/zh_CN.json')));
+      _this = this,
+      baseLocale = JSON.parse(fs.readFileSync(join(_this.sourceRoot(),
+        '../language/zh_CN.json')));
 
     //choose os locale
-    osLocale(function (err, locale) {
+    osLocale(function(err, locale) {
       //=> 'en_US'
       //ps.最终成型前只支持中文（简体）、中文（繁体）、英文（美式）
-      if(locale == 'zh_CN' || locale == 'zh_Hans'){
-        _this.locale = _.extend(baseLocale,JSON.parse(fs.readFileSync(join(_this.sourceRoot(), '../language/'+locale+'.json'))));
-      }else if(locale == 'zh_Hant' || locale == 'zh_HK' || locale == 'zh_MO'){
-        _this.locale = _.extend(baseLocale,JSON.parse(fs.readFileSync(join(_this.sourceRoot(), '../language/zh_Hant.json'))));
-      }else{
-        _this.locale = _.extend(baseLocale,JSON.parse(fs.readFileSync(join(_this.sourceRoot(), '../language/en_US.json'))));
+      if (locale == 'zh_CN' || locale == 'zh_Hans') {
+        _this.locale = _.extend(baseLocale, JSON.parse(fs.readFileSync(
+          join(_this.sourceRoot(), '../language/' + locale +
+            '.json'))));
+      } else if (locale == 'zh_Hant' || locale == 'zh_HK' || locale ==
+        'zh_MO') {
+        _this.locale = _.extend(baseLocale, JSON.parse(fs.readFileSync(
+          join(_this.sourceRoot(), '../language/zh_Hant.json'))));
+      } else {
+        _this.locale = _.extend(baseLocale, JSON.parse(fs.readFileSync(
+          join(_this.sourceRoot(), '../language/en_US.json'))));
       }
       // welcome message
       if (!_this.options['skip-welcome-message']) {
@@ -52,71 +58,79 @@ module.exports = yeoman.generators.Base.extend({
         _this.log(chalk.magenta(_this.locale['welcome']));
       }
 
-      var prompts = [
-        {
-          name: 'name',
-          message: _this.locale['name'],
-          default: 'myproject'
-        },{
-          name: 'description',
-          message: _this.locale['description'],
-          default: 'This is a nuclear package!'
-        },{
-          type: 'rawlist',
-          name: 'cssmodel',
-          message: _this.locale['cssmodel']['message'],
-          choices: [{
-            name: _this.locale['cssmodel']['choices']['less'],
-            value: 'includeLess'
-          },{
-            name: _this.locale['cssmodel']['choices']['sass'],
-            value: 'includeSass'
-          },{
-            name: _this.locale['cssmodel']['choices']['css'],
-            value: 'includeCss'
-          }]
-        },{
-          type: 'checkbox',
-          name: 'features',
-          message: _this.locale['features']['message'],
-          choices: [{
-            name: _this.locale['features']['choices']['bootstrap'],
-            value: 'includeBootstrap',
-            checked: false
-          },{
-            name: _this.locale['features']['choices']['jquery'],
-            value: 'includeJquery',
-            checked: true
-          },{
-            name: _this.locale['features']['choices']['modernizr'],
-            value: 'includeModernizr',
-            checked: true
-          }]
-        },{
-          type: 'rawlist',
-          name: 'devmodel',
-          message: _this.locale['devmodel']['message'],
-          choices: [{
-            name: _this.locale['devmodel']['choices']['templates'],
-            value: 'templates'
-          },{
-            name: _this.locale['devmodel']['choices']['fullstack'],
-            value: 'fullstack'
-          }]
-        }
-      ];
+      var prompts = [{
+        name: 'name',
+        message: _this.locale['name'],
+        default: 'myproject'
+      }, {
+        name: 'description',
+        message: _this.locale['description'],
+        default: 'This is a nuclear package!'
+      }, {
+        type: 'rawlist',
+        name: 'cssmodel',
+        message: _this.locale['cssmodel']['message'],
+        choices: [{
+          name: _this.locale['cssmodel']['choices']['less'],
+          value: 'includeLess'
+        }, {
+          name: _this.locale['cssmodel']['choices']['sass'],
+          value: 'includeSass'
+        }, {
+          name: _this.locale['cssmodel']['choices']['css'],
+          value: 'includeCss'
+        }]
+      }, {
+        type: 'checkbox',
+        name: 'features',
+        message: _this.locale['features']['message'],
+        choices: [{
+          name: _this.locale['features']['choices'][
+            'bootstrap'
+          ],
+          value: 'includeBootstrap',
+          checked: false
+        }, {
+          name: _this.locale['features']['choices']['jquery'],
+          value: 'includeJquery',
+          checked: true
+        }, {
+          name: _this.locale['features']['choices'][
+            'modernizr'
+          ],
+          value: 'includeModernizr',
+          checked: true
+        }]
+      }, {
+        type: 'rawlist',
+        name: 'devmodel',
+        message: _this.locale['devmodel']['message'],
+        choices: [{
+          name: _this.locale['devmodel']['choices'][
+            'templates'
+          ],
+          value: 'templates'
+        }, {
+          name: _this.locale['devmodel']['choices'][
+            'fullstack'
+          ],
+          value: 'fullstack'
+        }]
+      }];
 
-      _this.prompt(prompts, function (answers) {
+      _this.prompt(prompts, function(answers) {
         var features = answers.feature,
-            cssmodel = answers.cssmodel,
-            devmodel = answers.devmodel;
+          cssmodel = answers.cssmodel,
+          devmodel = answers.devmodel;
 
         function hasFeature(feat) {
           return features && features.indexOf(feat) !== -1;
         };
+
         function hasCssmodel(css) {
           return cssmodel && cssmodel.indexOf(css) !== -1;
         };
+
         function hasDevmodel(dev) {
           return devmodel && devmodel.indexOf(dev) !== -1;
         };
@@ -142,24 +156,24 @@ module.exports = yeoman.generators.Base.extend({
     });
   },
 
-  gruntfile: function () {
+  gruntfile: function() {
     this.template('Gruntfile.js');
   },
 
-  templatesJSON: function () {
+  templatesJSON: function() {
     this.template('templatesConfig.json');
   },
 
-  packageJSON: function () {
+  packageJSON: function() {
     this.template('_package.json', 'package.json');
   },
 
-  git: function () {
+  git: function() {
     this.template('gitignore', '.gitignore');
     this.copy('gitattributes', '.gitattributes');
   },
 
-  bower: function () {
+  bower: function() {
     var bower = {
       name: this._.slugify(this.appname),
 
@@ -170,10 +184,10 @@ module.exports = yeoman.generators.Base.extend({
 
     if (this.includeBootstrap && !this.includeSass) {
       bower.dependencies['bootstrap'] = '~3.2.0';
-    }else if(this.includeBootstrap && this.includeSass){
+    } else if (this.includeBootstrap && this.includeSass) {
       bower.dependencies['bootstrap-sass-official'] = '~3.2.0';
     } else {
-      if (this.includeJquery){
+      if (this.includeJquery) {
         bower.dependencies['jquery'] = '~1.11.1';
       }
       bower.dependencies['normalize.css'] = '~3.0.3';
@@ -187,25 +201,25 @@ module.exports = yeoman.generators.Base.extend({
     this.write('bower.json', JSON.stringify(bower, null, 2));
   },
 
-  jshint: function () {
+  jshint: function() {
     this.copy('jshintrc', '.jshintrc');
   },
 
-  editorConfig: function () {
+  editorConfig: function() {
     this.copy('editorconfig', '.editorconfig');
   },
 
-  mainStylesheet: function () {
-    if(this.includeLess){
-      this.template('main.less', 'app/less/main.less');
-    }else if(this.includeSass){
-      this.template('main.scss', 'app/scss/main.scss');
-    }else{
-      this.template('main.css', 'app/styles/main.css');
+  mainStylesheet: function() {
+    if (this.includeLess) {
+      this.template('main.less', 'dev/less/main.less');
+    } else if (this.includeSass) {
+      this.template('main.scss', 'dev/scss/main.scss');
+    } else {
+      this.template('main.css', 'dev/styles/main.css');
     }
   },
 
-  writeIndex: function () {
+  writeIndex: function() {
     this.indexFile = this.engine(
       this.readFileAsString(join(this.sourceRoot(), 'index.html')),
       this
@@ -242,26 +256,26 @@ module.exports = yeoman.generators.Base.extend({
       fileType: 'js',
       optimizedPath: 'scripts/main.js',
       sourceFileList: ['scripts/main.js'],
-      searchPath: ['app']
+      searchPath: ['dev']
     });
   },
 
-  app: function () {
-    this.directory('app');
-    this.mkdir('app/scripts');
-    this.mkdir('app/styles');
-    this.mkdir('app/images');
-    this.write('app/index.html', this.indexFile);
+  app: function() {
+    this.directory('dev');
+    this.mkdir('dev/scripts');
+    this.mkdir('dev/styles');
+    this.mkdir('dev/images');
+    this.write('dev/index.html', this.indexFile);
 
     if (this.coffee) {
-      this.copy('main.coffee', 'app/coffee/main.coffee');
+      this.copy('main.coffee', 'dev/coffee/main.coffee');
     } else {
-      this.copy('main.js', 'app/scripts/main.js');
+      this.copy('main.js', 'dev/scripts/main.js');
     }
   },
 
-  install: function () {
-    this.on('end', function () {
+  install: function() {
+    this.on('end', function() {
       this.invoke(this.options['test-framework'], {
         options: {
           'skip-message': this.options['skip-install-message'],
