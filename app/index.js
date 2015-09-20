c197ae26a7430f319e902ef742c56a9b40b43cfd
chinakids <494063570@qq.com>
2015-09-20 23:12:38 +0800

    初始化代码

--- app/index.js
+++ app/index.js
@@ -11,15 +11,7 @@ module.exports = yeoman.generators.Base.extend({
   constructor: function() {
     yeoman.generators.Base.apply(this, arguments);
 
-
-    // setup the test-framework property, Gruntfile template will need this
-    this.option('test-framework', {
-      desc: 'Test framework to be invoked',
-      type: String,
-      defaults: 'mocha'
-    });
-    this.testFramework = this.options['test-framework'];
-
+    //coffee
     this.option('coffee', {
       desc: 'Use CoffeeScript',
       type: Boolean,
